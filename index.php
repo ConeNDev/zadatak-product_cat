8b541e4429d69a8537095eea58abe95c4f24dd5d
ConeNDev <conedeveloper007@gmail.com>
2025-02-15 18:26:22 +0100

    fja za slanje responsa

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
-// Uključi ruter
-require_once 'router.php';
+require 'config.php';
 
-// Pokreni ruter
-$router = new Router();
-$router->handleRequest();
+header("Content-Type: application/json");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+//uri
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uriParts = array_values(array_filter(explode('/', $uri)));
+
+// sluzi za slanje odgovora u jsonu
+function sendResponse($data, $status = 200) {
+    http_response_code($status);
+    echo json_encode($data);
+    exit;
+}
